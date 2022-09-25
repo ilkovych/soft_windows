add25cdda2b13240f4bfea84cb365f7a2d1f2d43
Roman Serdiuk <51324307+budfy@users.noreply.github.com>
2022-09-26 02:17:28 +0300

    video-testimonials

--- src/components/page-blocks/testimonials/_testimonials.js
+++ src/components/page-blocks/testimonials/_testimonials.js
@@ -4,7 +4,10 @@ function testimonialSliders() {
       navigation: {
     nextEl: '.testimonials__video-slider .swiper-button-next',
     prevEl: '.testimonials__video-slider .swiper-button-prev',
-  },
+    },
+    pagination: {
+        el:'.testimonials__video-slider .swiper-pagination'
+      }
   });
   const googleSlider = new Swiper(".testimonials__google-wrapper .swiper", {});
 
